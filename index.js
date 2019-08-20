91a2511fbf5f7539df44f599432598bb4d8bb525
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-08-20 13:36:06 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { FastExpress } = require("@karakulov-web-dev/fast-express");
+const Jimp = require("jimp");
+const axios = require("axios");
+
+class ForpostAppApi extends FastExpress {
+  async image(req) {
+    const { url } = req.query;
+    if (!url) {
+      return "url is undefined";
+    }
+
+    try {
+      let result = await axios.get(url, { responseType: "arraybuffer" });
+      let image = await Jimp.read(result.data);
+      let newBuffer = await image.getBufferAsync(Jimp.MIME_PNG);
+      return newBuffer;
+    } catch (e) {
+      console.log(e);
+      return "error";
+    }
+  }
+}
+
+new ForpostAppApi(8009);
